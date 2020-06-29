56a966d7230a9a6db04fe8c9de3c155a2c80e2bd
azrehman <abdullahzr@vt.edu>
2020-06-29 15:52:49 -0400

    changed saving and loading dat to files to just be imported by rnn.py instead

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,13 @@ import string
 import re
 import numpy as np
 import pandas as pd
+
 from stop_words import get_stop_words
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 
 print('begin processing...')
 
@@ -13,6 +18,7 @@ print('begin processing...')
 df = pd.read_csv('stock_data.csv',)
 # shuffle rows in data frame
 df = df.sample(frac=1).reset_index(drop=True)
+
 
 # seperate into featureset and classes
 x_data = df['Text'].to_numpy()
@@ -29,9 +35,10 @@ y_data[y_data == -1] = 0
 # one hot encode classes
 y_data = make_one_hot(y_data, 2)
 
+
 # split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(
-    x_data, y_data, test_size=0.2)
+    x_data, y_data, test_size=0.25)
 
 ### data processing ###
 
@@ -57,9 +64,13 @@ def remove_case_white_space(text):
 
 
 # remove stop words ex) for, is, my
-def remove_stop_words(arr):
+def remove_stop_words(text):
     stops = get_stop_words('english') + ['user', '\n']
-    return np.array(list(filter(lambda word: word not in stops, arr)))
+    stops.remove('down')
+    stops.remove('should')
+    # return np.array(list(filter(lambda word: word not in stops, arr)))
+    return ' '.join([word for word in text.split()
+                     if word not in stops])
 
 
 # remove empty strings if left in for some reason
@@ -68,27 +79,61 @@ def remove_empty(arr):
 
 
 # use functions to preprocess data
-X_train = [remove_URL(text) for text in X_train]
-X_test = [remove_URL(text) for text in X_test]
+X_train = np.array([remove_URL(text) for text in X_train])
+X_test = np.array([remove_URL(text) for text in X_test])
 
-X_train = [remove_punct(text) for text in X_train]
-X_test = [remove_punct(text) for text in X_test]
+X_train = np.array([remove_punct(text) for text in X_train])
+X_test = np.array([remove_punct(text) for text in X_test])
 
-X_train = [remove_case_white_space(text) for text in X_train]
-X_test = [remove_case_white_space(text) for text in X_test]
+X_train = np.array([remove_case_white_space(text) for text in X_train])
+X_test = np.array([remove_case_white_space(text) for text in X_test])
 
-X_train = remove_stop_words(X_train)
-X_test = remove_stop_words(X_test)
+
+X_train = np.array([remove_stop_words(text) for text in X_train])
+X_test = np.array([remove_stop_words(text) for text in X_test])
+
+# X_train = remove_stop_words(X_train)
+# X_test = remove_stop_words(X_test)
 
 X_train = remove_empty(X_train)
 X_test = remove_empty(X_test)
 
-# saving featuresets
-np.save('processed_data/X_train.npy', X_train)
-np.save('processed_data/X_test.npy', X_test)
 
-# saving classes
-np.save('processed_data/y_train.npy', y_train)
-np.save('processed_data/y_test.npy', y_test)
+max_seq_length = 30
 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+word_index = tokenizer.word_index
+
+# tokenize training data
+X_train = tokenizer.texts_to_sequences(X_train)
+# pad training data
+X_train = pad_sequences(
+    X_train, maxlen=max_seq_length, padding='post', truncating='post'
+)
+
+# tokenize test data
+X_test = tokenizer.texts_to_sequences(X_test)
+# pad test data
+X_test = pad_sequences(
+    X_test, maxlen=max_seq_length, padding='post', truncating='post'
+)
+
+reverse_word_index = dict([(value, key)
+                           for (key, value) in word_index.items()])
+
+# method to go from tokenized sequences back to words
+
+
+def decode(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+# # saving featuresets
+# np.save('processed_data/X_train.npy', X_train)
+# np.save('processed_data/X_test.npy', X_test)
+
+# # saving classes
+# np.save('processed_data/y_train.npy', y_train)
+# np.save('processed_data/y_test.npy', y_test)
 print('done processing')
